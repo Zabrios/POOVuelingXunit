380c6805fe4c0b56c33c6d4c3801fe9c12876642
Zabrios <Zabrios@users.noreply.github.com>
2018-06-25 10:58:49 +0200

    Initial commit

---
+++ POOVuelingXunit.Integration.Tests/CalculadoraTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Xunit;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using POOVueling;
+
+namespace POOVuelingXunit.Integration.Tests
+{
+    public class CalculadoraTest
+    {
+        ICalculadora iCalculadora = new Calculadora();
+
+        [Fact]
+        public void DivisionTest()
+        {
+            var result = iCalculadora.Division(4, 2);
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void MultiplicacionTest()
+        {
+            var result = iCalculadora.Multiplicacion(4, 2);
+            Assert.Equal(8, result);
+        }
+
+        [Fact]
+        public void SumaTest()
+        {
+            var result = iCalculadora.Suma(4, 2);
+            Assert.Equal(6, result);
+        }
+
+        [Fact]
+        public void RestaTest()
+        {
+            var result = iCalculadora.Resta(4, 2);
+            Assert.Equal(2, result);
+        }
+    }
+}
---
+++ POOVuelingXunit.Unit.Tests/CalculadoraTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Xunit;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using POOVueling;
+
+namespace POOVuelingXunit.Unit.Tests
+{
+
+    public class CalculadoraTests
+    {
+        ICalculadora calculadora = new Calculadora();
+
+        [Theory]
+        [InlineData(4, 2, 2)]
+        [InlineData(18, 2, 9)]
+        public void DivisionTest(int num1, int num2, int expected)
+        {
+            //var calculadora = new Calculadora();
+            var result = calculadora.Division(num1, num2);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(4, 2, 8)]
+        [InlineData(18, 2, 36)]
+        public void MultiplicacionTest(int num1, int num2, int expected)
+        {
+            //var calculadora = new Calculadora();
+            var result = calculadora.Multiplicacion(num1, num2);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(4, 2, 2)]
+        [InlineData(18, 2, 16)]
+        public void RestaTest(int num1, int num2, int expected)
+        {
+            var result = calculadora.Resta(num1, num2);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(4, 2, 6)]
+        [InlineData(18, 2, 20)]
+        public void SumaTest(int num1, int num2, int expected)
+        {
+            var result = calculadora.Suma(num1, num2);
+            Assert.Equal(expected, result);
+        }
+    }
+}
